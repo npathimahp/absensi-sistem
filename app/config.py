f4a85dc9d92f23f3e5087c1108c7cdfb95bddb24
npathimahp <npathimahp@gmail.com>
2025-05-06 19:47:59 +0700

    first commit

---
+++ app/config.py
@@ -0,0 +1,10 @@
+# Cache for encoded faces
+ENCODE_FILE = "EncodeFile.p"  # Path to the encoded faces file
+
+# Secret key for Flask session
+SECRET_KEY = "your_secret_key_here"  # Change this to a random string
+
+# Firebase configuration details
+FIREBASE_CREDENTIALS = "serviceAccountKey.json"  # Path to the service account key
+DATABASE_URL = "https://faceabsence-743dd-default-rtdb.firebaseio.com/"  # URL to the Firebase Realtime Database
+STORAGE_BUCKET = "faceabsence-743dd.appspot.com"  # URL to the Firebase Storage Bucket
