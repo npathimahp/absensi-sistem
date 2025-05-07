3c402886b2501d5514e82f396bd10232e9f26d63
Nur Patimah <95556646+npathimahp@users.noreply.github.com>
2025-05-07 12:48:18 +0700

    Update initial_database.py

--- misc/initial_database.py
+++ misc/initial_database.py
@@ -3,11 +3,8 @@ import json
 import firebase_admin
 from firebase_admin import credentials, db
 
-if os.getenv("GOOGLE_CREDENTIALS"):
-    cred_dict = json.loads(os.environ["GOOGLE_CREDENTIALS"])
-    cred = credentials.Certificate(cred_dict)
-else:
-    cred = credentials.Certificate("serviceAccountKey.json")
+cred_dict = json.loads(os.getenv("FIREBASE_CREDENTIALS_JSON"))
+cred = credentials.Certificate(cred_dict)
     
 firebase_admin.initialize_app(
     cred,
